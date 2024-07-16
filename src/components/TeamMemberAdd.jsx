c3c6c872a9da280db5d05afc7adebedc42356316
Asutosh Pradhan <pasutosh15@gmail.com>
2024-07-16 14:45:03 +0530

    invite modal chnage

--- src/components/TeamMemberAdd.jsx
+++ src/components/TeamMemberAdd.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import axios from "axios";
 import { DataContext } from "@/context/dataContext";
-import User from "@/assets/dropz.svg";
+import User from "@/assets/inviteUser.svg";
 import Cross from "@/assets/circlecross.svg";
 import Success from "@/assets/success.svg";
 export default function TeamMemberAdd({ isOpen, onClose }) {
@@ -44,57 +44,56 @@ export default function TeamMemberAdd({ isOpen, onClose }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-[2000]">
       {!success ? (
-        <div className="bg-white px-[32px] py-[24px] rounded-lg border w-[544px]">
-          <div className="flex items-center justify-between mb-[32px]">
-            <h2 className="font-semibold text-[24px] leading-[32px]">
-              Invite Team member
+        <div className="bg-white px-[24px] pt-[20px] pb-[32px] rounded-[12px] border w-[367px] border-[#C9C5EB80]">
+          <div className="flex items-center justify-between mb-[8px]">
+            <h2
+              className="font-semibold text-[18px] leading-[21.48px] text-[#100F1F]"
+              style={{ letterSpacing: "-4%" }}
+            >
+              Invite Team Member
             </h2>
             <img
               src={Cross.src}
               alt=""
               onClick={resetForm}
-              className="cursor-pointer"
+              className="cursor-pointer h-[16px] w-[16px]"
             />
           </div>
+          <div
+            style={{ borderTop: "1px solid #C9C5EB4D" }}
+            className="mb-[20px]"
+          ></div>
           <form onSubmit={handleSubmit}>
-            <div className="mb-[32px] flex border-[2px] rounded-[12px] p-[16px] items-center w-full">
+            <div className="mb-[12px] flex border-[2px] rounded-[4px] py-[10px] px-[12px] items-center w-full">
               <img src={User.src} alt="" />
               <input
-                className="appearance-none w-full outline-none px-3 text-[16px] text-[#87898E] leading-[24px] font-medium"
+                className="appearance-none w-full outline-none px-[8px] text-[12px] text-[#100F1F] leading-[14.32px] text-opacity-60"
                 id="addressess"
                 name="addressess"
                 type="text"
                 value={walletAddresses}
                 onChange={handleWalletAddressesChange}
-                placeholder="Wallet addresses separated by commas"
+                placeholder="Enter Wallet address"
               />
             </div>
-            <div className="flex items-center justify-between gap-4">
-              <button
-                className="flex-1 text-[#23262F] py-[20px] rounded-full focus:outline-none focus:shadow-outline"
-                type="button"
-                onClick={resetForm}
-              >
-                Cancel
-              </button>
-              <button
-                className="gradientBackground flex-1 hover:bg-orange-700 text-[#FCFCFD] py-[20px] rounded-full focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Save
-              </button>
-            </div>
+
+            <button
+              className="w-full gradientBackground hover:bg-orange-700 text-[#FFFFFF] py-[11.5PX] rounded-[4px] focus:outline-none focus:shadow-outline font-semibold"
+              type="submit"
+            >
+              Request Access
+            </button>
           </form>
         </div>
       ) : (
-        <div className="bg-white relative px-[32px] py-[24px] rounded-lg border w-[544px]">
+        <div className="bg-white relative px-[32px] py-[24px] rounded-[12px] border w-[367px] h-[191px]">
           <img
             src={Cross.src}
             alt=""
             onClick={resetForm}
             className="cursor-pointer absolute right-4 top-4"
           />
-          <img src={Success.src} alt="" />
+          <img src={Success.src} alt="" className="mt-[-90px]"/>
         </div>
       )}
     </div>
